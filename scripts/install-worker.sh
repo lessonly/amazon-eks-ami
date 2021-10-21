ad6ce720cc687636cfcfbf6ca2b556f8f326908c
Tyler Henrichs <tyler.henrichs@lessonly.com>
2021-10-21 12:20:36 -0400

    remove echo of rapid7 token and also add sudo for removal of the rapid7 bootstrap.cfg file

--- scripts/install-worker.sh
+++ scripts/install-worker.sh
@@ -317,10 +317,9 @@ sudo sh ./inspector_install.sh
 
 # https://docs.rapid7.com/insight-agent/using-a-token
 chmod u+x  /tmp/lessonly/agent_installer.sh
-echo RAPID7_TOKEN us:$RAPID7_TOKEN
 sudo  /tmp/lessonly/agent_installer.sh install --token us:$RAPID7_TOKEN
 # https://docs.rapid7.com/insight-agent/virtualization/
-rm -rf /opt/rapid7/ir_agent/components/bootstrap/common/bootstrap.cfg
+sudo rm -rf /opt/rapid7/ir_agent/components/bootstrap/common/bootstrap.cfg
 
 ################################################################################
 ### SOPHOS INSTALL #############################################################
