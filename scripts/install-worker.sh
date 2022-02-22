7d2b26659c06e11341c100146270d70a2360aa13
Tyler Henrichs <tyler.henrichs@lessonly.com>
2022-02-22 16:12:06 -0500

    systemctl not service

--- scripts/install-worker.sh
+++ scripts/install-worker.sh
@@ -335,7 +335,7 @@ sudo  /tmp/lessonly/agent_installer.sh install --token us:$RAPID7_TOKEN
 sudo rm -rf /opt/rapid7/ir_agent/components/bootstrap/common/bootstrap.cfg
 
 # Disable auditd to allow Rapid7 Agent to work properly
-service auditd stop
+sudo systemctl stop auditd
 sudo systemctl disable auditd
 chkconfig auditd off
 
